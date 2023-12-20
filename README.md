5f3e53c02275810f7b355de57c1ebd110d3c7056
OhK <85354867+OhKieran@users.noreply.github.com>
2023-12-20 23:59:49 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# instrument_app
+
+Simple musical app
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MusicApp());
+
+class MusicApp extends StatelessWidget {
+  const MusicApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
